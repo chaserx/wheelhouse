e10425652a51397a729db7a7dbc1518eeafd0e8b
Chase Southard <chase.southard@gmail.com>
2015-12-20 16:13:46 -0500

    refactor round 1. fight.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -10,26 +10,16 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find_or_initialize_by(name: org_name)
     @client = GithubClient.new
     if @organization.new_record? # or record is outdated
-      if data = @client.fetch_org_info(@organization.name)
-        if @organization.set_github_attrs(data)
-          if @organization.save
-            @members = @client.fetch_org_member_list(@organization.login)
-            @members.each do |member_data|
-              member_args = { github_login: member_data.login }
-              member = Member.find_or_initialize_by(member_args)
-              # if new member record or member record outdated
-              member.set_github_attrs(member_data)
-              member.organizations << @organization
-              member.save
-              SkillFetcherJob.perform_later(member.id)
-            end
-            redirect_to @organization
-          else
-            not_found # being lazy, but this is really could not save
-          end
+      data = @client.fetch_org_info(@organization.name)
+      if @organization.set_github_attrs(data)
+        if @organization.save
+          grab_members(@organization)
+          flash[:notice] = "We're busy gathering data from GitHub." +
+                           ' This might take a while. You may want to ' +
+                           'Refresh often.'
+          redirect_to @organization
         else
-          # if we get here, it's real bad
-          not_found
+          save_failed
         end
       else
         not_found
@@ -58,5 +48,26 @@ class OrganizationsController < ApplicationController
     flash[:alert] = 'Sorry. We could not find or create organization:' \
                     " #{params[:organization][:name]}"
     redirect_to root_path
+  end
+
+  def save_failed
+    flash[:alert] = 'Failed to create organization: ' +
+                      @organization.errors.full_messages.join(', ')
+    redirect_to root_path
+  end
+
+  def grab_members(org)
+    @client = GithubClient.new
+    @members = @client.fetch_org_member_list(org.login)
+    @members.each do |member_data|
+      member_args = { github_login: member_data.login }
+      member = Member.find_or_initialize_by(member_args)
+      if member.new_record? # or member record outdated
+        member.set_github_attrs(member_data)
+        member.organizations << org
+        member.save
+        SkillFetcherJob.perform_later(member.id)
+      end
+    end
   end
 end
