b90a02a1a2e2d639635b81788aca692eb1ffc25a
Chase Southard <chase.southard@gmail.com>
2015-12-22 08:08:22 -0500

    fix staleness and uniqueness problems
    not updating members properly

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
-  has_and_belongs_to_many :organizations
+  has_and_belongs_to_many :organizations, -> { uniq }
   has_many :languages
 
   def set_github_attrs(member_info)
@@ -8,5 +8,9 @@ class Member < ActiveRecord::Base
     self.avatar_url = member_info[:avatar_url]
     self.profile_url = member_info[:html_url]
     self.last_fetched_at = Time.zone.now
+  end
+
+  def stale?
+    Time.zone.now.to_i - updated_at.to_i > 30.seconds.to_i
   end
 end
