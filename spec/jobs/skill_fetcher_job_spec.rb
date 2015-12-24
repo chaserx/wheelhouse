f9de5e7b0e0864162d02a2792b02ef1491b87831
Chase Southard <chase.southard@gmail.com>
2015-12-23 19:45:27 -0500

    appease thy rubocop

--- spec/jobs/skill_fetcher_job_spec.rb
+++ spec/jobs/skill_fetcher_job_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe SkillFetcherJob, type: :job do
   describe '#fetch_org_info' do
-    let(:make_request) {
-      ->{
+    let(:make_request) do
+      lambda do
         VCR.use_cassette('github_organization') do
           GithubClient.new.fetch_org_info('openlexington')
         end
-      }
-    }
+      end
+    end
 
     it 'returns a resource' do
       expect(make_request.call).to be_a(Sawyer::Resource)
@@ -20,13 +20,13 @@ RSpec.describe SkillFetcherJob, type: :job do
   end
 
   describe '#fetch_org_member_list' do
-    let(:make_request) {
-      ->{
+    let(:make_request) do
+      lambda do
         VCR.use_cassette('github_organization') do
           GithubClient.new.fetch_org_member_list('openlexington')
         end
-      }
-    }
+      end
+    end
 
     it 'returns an array' do
       expect(make_request.call).to be_a(Array)
@@ -38,24 +38,24 @@ RSpec.describe SkillFetcherJob, type: :job do
   end
 
   describe '#fetch_member_languages' do
-    let(:make_request) {
-      ->{
+    let(:make_request) do
+      lambda do
         VCR.use_cassette('chaserx') do
           GithubClient.new.fetch_member_languages('chaserx')
         end
-      }
-    }
+      end
+    end
 
     it 'returns a hash' do
       expect(make_request.call).to be_a(Hash)
     end
 
     it 'returns a hash with language keys and byte values' do
-      expected_hsh = {:Ruby => 969926, :JavaScript => 959973, :CSS => 264713,
-                      :HTML => 63030, :VimL => 17709, :Swift => 12933,
-                      :CoffeeScript => 12107, :"Objective-C" => 9824,
-                      :Shell => 6939, :Arduino => 6044, :OpenSCAD => 2306,
-                      :PHP => 269}
+      expected_hsh = { 'Ruby': 969_926, 'JavaScript': 959_973, 'CSS': 264_713,
+                       'HTML': 63_030, 'VimL': 17_709, 'Swift': 12_933,
+                       'CoffeeScript': 12_107, 'Objective-C': 9824,
+                       'Shell': 6939, 'Arduino': 6044, 'OpenSCAD': 2306,
+                       'PHP': 269 }
       expect(make_request.call).to eq(expected_hsh)
     end
   end
